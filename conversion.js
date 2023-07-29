538a5b1cb7724458fc8c00c911f102743b7c1889
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-29 18:22:59 +0600

    also code added

---
+++ conversion.js
@@ -0,0 +1,15 @@
+var price1='31';
+var price2='10';
+var totalPrice=price1+price2;
+// console.log(totalPrice);
+var price1='31';
+var price2=10;
+var totalPrice=price1+price2;
+console.log(totalPrice);
+// converts string to number
+console.log(price2);
+var price1Number =parseInt (price1);
+console.log(price1Number);
+var gpa='3.41';
+var gpaNumnber= parseFloat(gpa);
+console.log(gpaNumnber);
