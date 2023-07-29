69526ad68d908fd2550ddc7c2aac32ac6eb396d3
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-29 11:18:44 +0600

    operation

---
+++ operation.js
@@ -0,0 +1,18 @@
+// var onionPrice=20;
+// var eggPrice=10;
+// console.log(eggPrice);
+// console.log(onionPrice);
+// console.log(onionPrice+eggPrice);
+
+// var totalPrice=onionPrice+eggPrice;
+// console.log(totalPrice);
+
+var orangePrice=30;
+var quantity=20;
+var totalCost=orangePrice*quantity;
+console.log(totalCost);
+// division
+var money=500;
+var player=11;
+var eacPlayer=money/player;
+console.log(eacPlayer);
---
+++ variable-name.js
@@ -0,0 +1,4 @@
+//1. variable name cannot be reserved word 
+ userhomeAddress="andor kailla bandorbon";//camel case
+ UserHomeAddress="andor kailla bandorbon";//pascal case
+ user_home_Address="andor kailla bandorbon";//snake case
