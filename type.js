6ddd7aed44ab05a7612ed83b4e894c1ba8af2133
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-29 18:37:34 +0600

    type code added

---
+++ type.js
@@ -0,0 +1,15 @@
+var price=100;
+// console.log(typeof price);
+var price='100';
+// console.log(typeof price);
+var isHappy=true
+// console.log(typeof isHappy);
+var romantic;
+// console.log(typeof romantic);
+var num1=0.1;
+var num2=0.2;
+var totalNum=0.1+0.2;
+// totalNum=parseFloat(totalNum);
+ totalNum=totalNum.toFixed(2);
+ totalNum=parseFloat(totalNum);
+console.log(totalNum);
