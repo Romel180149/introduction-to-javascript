d3a6bdcc40c301dcb400a7f527e18dac1e7973bb
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-29 18:13:28 +0600

    concatetation added

---
+++ concatetation.js
@@ -0,0 +1,8 @@
+var price1=10;
+var price2=20;
+var totalPrice=price1+price2;
+// console.log(totalPrice);
+var firstName='jennie';
+var lastName='kim';
+var fullName=firstName+' '+lastName;
+console.log(fullName);
