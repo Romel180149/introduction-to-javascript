85aec1e6f412319ff41ee5315bf1fdd20a955a76
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-29 12:02:36 +0600

    shorthand aded

---
+++ shorthand.js
@@ -0,0 +1,15 @@
+var tomatoPrice=25;
+
+tomatoPrice=30;
+
+var price1=31;
+var price2=10;
+//var price1=35;
+price1=price1+10;
+console.log(price1);
+price1 +=10;
+console.log(price1);
+price1 *=10;
+console.log(price1);
+price1/=5;
+console.log(price1);
