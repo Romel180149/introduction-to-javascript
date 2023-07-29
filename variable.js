17b5202badea6607c255c8d1a1d959b76088a542
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-29 09:51:09 +0600

    string booen added

--- variable.js
+++ variable.js
@@ -1,6 +1,13 @@
 // console.log(24)
+// numeric
 var price =12;
-var location='sodor uddin';
-var special="alia batt";
 var age=23;
 var temperature=38;
+
+// string
+var location='sodor uddin';
+var special="alia batt";
+//  boolean
+var serious=true;
+var isFullmark=true;
+var IsSingle=false;
